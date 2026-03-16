0c52d9a16e220f7c0c746ce9c26f82376a3232ee
leschwe <leandro.schweiss@gmail.com>
2026-03-16 10:46:49 +0100

    Update config: 2026-03-16 10:46

--- installer.sh
+++ installer.sh
@@ -3,14 +3,21 @@
 sudo dnf install zsh starship jetbrains-mono-fonts-all -y
 
 # get oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
-# copy conbfig files
-cp ./zshrc ~/.zshrc
+mkdir -p ~/.config
+mkdir -p ~/.local/share/konsole
+
+# copy config files
+cp ./.zshrc ~/.zshrc
 cp ./starship.toml ~/.config/starship.toml
-cp ./custom ~/.oh-my-zsh
-sudo cp -r ./ls-std.profile ~/.local/share/konsole
+cp -r ./custom/* ~/.oh-my-zsh/custom/
+cp ./ls-std.profile ~/.local/share/konsole/
 
 
 # use zsh as std
 sudo chsh -s $(which zsh) $USER
+
+echo "Setup completed, restart console"
--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cp ~/.zshrc .
-cp ~/.config/starship.toml .
-cp -r ~/.oh-my-zsh/custom .
-sudo cp -r ~/.local/share/konsole/ls-std.profile .
+cp ~/.zshrc ./zshrc
+cp ~/.config/starship.toml ./starship.toml
+cp -r ~/.oh-my-zsh/custom/* ./custom/
+cp ~/.local/share/konsole/ls-std.profile ./ls-std.profile
+
+
+git add .
+git commit -m "Update config: $(date +'%Y-%m-%d %H:%M')"
+git push origin master
